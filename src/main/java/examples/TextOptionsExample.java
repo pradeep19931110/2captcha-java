0195f4af940579497bcda790e098caedba9f27f8
pradeep19931110 <93323514+pradeep19931110@users.noreply.github.com>
2021-10-30 15:33:20 +0530

    Update TextOptionsExample.java
    Hello

--- src/main/java/examples/TextOptionsExample.java
+++ src/main/java/examples/TextOptionsExample.java
@@ -6,12 +6,13 @@ import com.twocaptcha.captcha.Text;
 public class TextOptionsExample {
 
     public static void main(String[] args) {
-        TwoCaptcha solver = new TwoCaptcha("YOUR_API_KEY");
+        TwoCaptcha solver = new TwoCaptcha("YOUR_API_KEY");94db4b8b05d25b616c56a135c3248960
+
 
         Text captcha = new Text();
         captcha.setText("If tomorrow is Saturday, what day is today?");
         captcha.setLang("en");
-
+friday
         try {
             solver.solve(captcha);
             System.out.println("Captcha solved: " + captcha.getCode());
